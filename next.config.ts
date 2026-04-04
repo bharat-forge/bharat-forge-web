9fa9726e9a4d7df7fbb61f20119c9ff41839dae2
subhashankarb <subhashankarbehera@gmail.com>
2026-04-04 19:43:44 +0530

    Fixed bugs #1

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,17 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      }
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' }
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://bharatforge.onrender.com/api/:path*',
+      },
+    ];
   },
 };
 
